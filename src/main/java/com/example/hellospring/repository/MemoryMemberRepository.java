7b6eacea688ad65c146a58563f524a11d708d6a2
zooneon <snsxhdz@naver.com>
2021-03-17 21:25:20 +0900

    회원 도메인과 레포지토리 생성

---
+++ src/main/java/com/example/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package com.example.hellospring.repository;
+
+import com.example.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        // null이 반환될 수도 있을 경우 ofNullable로 감싸줌
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
