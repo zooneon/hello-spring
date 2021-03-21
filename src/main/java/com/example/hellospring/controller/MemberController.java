ad80ed88e50b321983240a824c05972fad79af50
zooneon <snsxhdz@naver.com>
2021-03-21 14:49:49 +0900

    등록 기능 추가

--- src/main/java/com/example/hellospring/controller/MemberController.java
+++ src/main/java/com/example/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.example.hellospring.controller;
 
+import com.example.hellospring.domain.Member;
 import com.example.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,21 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        memberService.join(member);
+
+        // 홈 화면으로 다시 보냄
+        return "redirect:/";
     }
 }
